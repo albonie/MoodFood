e1bdb0e46161242312e853d4319e22b1a4209d7e
Krzysztof Ciepłucha <krzys.cieplucha@gmail.com>
2020-11-15 01:38:31 +0100

    stworzyłem activity do wyboru programu

--- app/src/main/java/com/example/moodfood/WyborProgramu.kt
+++ app/src/main/java/com/example/moodfood/WyborProgramu.kt
@@ -2,10 +2,13 @@ package com.example.moodfood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_wybor_programu.*
 
 class WyborProgramu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wybor_programu)
+        lodowka.text = "Lodówka"
+        chrapka.text = "Chrapka"
     }
 }
