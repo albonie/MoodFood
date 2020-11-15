1372df18daa39b40dd4519d2fbf4cb0810c74c91
KhalSCI <kubapp23@gmail.com>
2020-11-15 16:52:23 +0100

    Przepis++

--- app/src/main/java/com/example/moodfood/Przepisy.kt
+++ app/src/main/java/com/example/moodfood/Przepisy.kt
@@ -2,10 +2,16 @@ package com.example.moodfood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_przepisy.*
 
 class Przepisy : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_przepisy)
+        BADD.setOnClickListener {
+            val message = Toast.makeText(applicationContext,"Dodano!",Toast.LENGTH_LONG)
+            message.show()
+        }
     }
 }
