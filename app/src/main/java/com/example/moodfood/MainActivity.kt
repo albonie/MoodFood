f418aa99a7bc69d1947f076fcd806cd22643fdaa
albonie <56626306+albonie@users.noreply.github.com>
2020-11-15 21:10:31 +0100

    przełączanie się pomiędzy przepisami oraz wyborem programu

--- app/src/main/java/com/example/moodfood/MainActivity.kt
+++ app/src/main/java/com/example/moodfood/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.moodfood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonPrzepis.setOnClickListener{
+            val intent = Intent(this, Przepisy::class.java)
+            startActivity(intent)
+        }
+        buttonProgram.setOnClickListener {
+            val intent = Intent(this, WyborProgramu::class.java)
+            startActivity(intent)
+        }
     }
 }
