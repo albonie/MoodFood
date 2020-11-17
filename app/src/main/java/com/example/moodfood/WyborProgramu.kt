be7e81b929e2eda2f544c03a4a35deb6c4f30ca3
Krzysztof Ciepłucha <krzys.cieplucha@gmail.com>
2020-11-17 13:56:07 +0100

    animacje do WyborProgramu.kt

--- app/src/main/java/com/example/moodfood/WyborProgramu.kt
+++ app/src/main/java/com/example/moodfood/WyborProgramu.kt
@@ -2,11 +2,16 @@ package com.example.moodfood
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_wybor_programu.*
 
 class WyborProgramu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wybor_programu)
+        val wjazd = AnimationUtils.loadAnimation(this, R.anim.wjazd)
+        val wjazdZLewej = AnimationUtils.loadAnimation(this, R.anim.wjazd_z_lewej)
+        lodowka.startAnimation(wjazd)
+        chrapka.startAnimation(wjazdZLewej)
     }
 }
