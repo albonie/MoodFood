aea1a6cbef9313bebe24a777396afb79c20972e7
albonie <56626306+albonie@users.noreply.github.com>
2020-11-17 13:52:08 +0100

    Zaanimowanie MainActivity.kt

--- app/src/main/java/com/example/moodfood/MainActivity.kt
+++ app/src/main/java/com/example/moodfood/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.moodfood
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val wjazd = AnimationUtils.loadAnimation(this, R.anim.wjazd)
+        val wjazd2 = AnimationUtils.loadAnimation(this, R.anim.wjazd2)
+        val wjazdZLewej = AnimationUtils.loadAnimation(this, R.anim.wjazd_z_lewej)
+        val wjazdZLewej2 = AnimationUtils.loadAnimation(this, R.anim.wjazd_z_lewej2)
+        buttonOpcje.startAnimation(wjazd)
+        buttonProgram.startAnimation(wjazd2)
+        buttonPrzepis.startAnimation(wjazdZLewej)
+        buttonInfo.startAnimation(wjazdZLewej2)
         buttonPrzepis.setOnClickListener{
             val intent = Intent(this, Przepisy::class.java)
             startActivity(intent)
