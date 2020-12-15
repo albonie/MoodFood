cb821c63516258c6bce9903f5d990dfe6bcae17b
KhalSCI <kubapp23@gmail.com>
2020-12-15 15:34:54 +0100

    UsuwankoDzwiekuZ O Aplikacji

--- app/src/main/java/com/example/moodfood/oAplikacji.kt
+++ app/src/main/java/com/example/moodfood/oAplikacji.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class oAplikacji : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_opcyje)
+        setContentView(R.layout.activity_o_aplikacji)
         val actionbar = supportActionBar
         actionbar!!.title = "O Aplikacji"
         actionbar.setDisplayHomeAsUpEnabled(true)
