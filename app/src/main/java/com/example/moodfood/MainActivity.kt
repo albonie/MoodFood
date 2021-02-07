1bedf64f5d346660f8e007656501a3589ceda3b1
albonie <56626306+albonie@users.noreply.github.com>
2021-02-08 00:39:59 +0100

    Update 08.02.2021

--- app/src/main/java/com/example/moodfood/MainActivity.kt
+++ app/src/main/java/com/example/moodfood/MainActivity.kt
@@ -11,15 +11,16 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val wjazd = AnimationUtils.loadAnimation(this, R.anim.wjazd)
         val wjazd2 = AnimationUtils.loadAnimation(this, R.anim.wjazd2)
         val wjazdZLewej = AnimationUtils.loadAnimation(this, R.anim.wjazd_z_lewej)
-        val wjazdZLewej2 = AnimationUtils.loadAnimation(this, R.anim.wjazd_z_lewej2)
         buttonProgram.startAnimation(wjazd2)
         buttonPrzepis.startAnimation(wjazdZLewej)
         buttonPrzepis.setOnClickListener{
+
             val intent = Intent(this, Przepisy::class.java)
             startActivity(intent)
         }
